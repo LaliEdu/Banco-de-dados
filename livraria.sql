bf91e8ab004cbb4dd889583af2e4bf75bd16f641
Eduarda Alves <alvesneivam1203@gmail.com>
2025-03-17 11:44:31 -0300

    Update 17.03

---
+++ livraria.sql
@@ -0,0 +1,36 @@
+# Banco de Dados
+CREATA DATABASE Livraria
+
+CREATE TABLE Livros (
+    ID_Livro SERIAL PRIMARY KEY,
+    Titulo VARCHAR(255),
+    Autor VARCHAR(255),
+    Editora VARCHAR(255),
+    Endereco_Editora VARCHAR(255),
+    Categoria VARCHAR(255)
+);
+
+CREATE TABLE Editora (
+    ID_Editora SERIAL PRIMARY KEY,
+    Nome VARCHAR(255),
+    Endereco VARCHAR(255)
+);
+
+ALTER TABLE Livros
+ADD COLUMN ID_Editora INT REFERENCES Editoras (ID_Editora);
+
+ALTER TABLE Livros
+DROP COLUMN Editora,
+DROP COLUMN Endereco_Editora;
+
+CREATE TABLE Categorias (
+    ID_Categoria SERIAL PRIMARY KEY,
+    Nome VARCHAR(255)
+);
+
+ALTER TABLE Livros
+ADD COLUMN ID_Categoria INT REFERENCES
+Categorias(ID_Categoria);
+
+ALTER TABLE Livros
+DROP COLUMN Categoria;
