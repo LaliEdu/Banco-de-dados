38e9cffb01864cba2044771eb4156d8b481a6c32
Eduarda Alves <alvesneivam1203@gmail.com>
2025-03-24 11:45:31 -0300

    Update

---
+++ Atualizar_arquivos.sql
@@ -0,0 +1,8 @@
+select * from livros l ;
+
+update livros set preco = 52.50 where id_livro = 18;
+
+select * from livros l where id_livro = 18;
+
+-Deletar dados da tabela-
+delete from livros where id_livro = 18;
